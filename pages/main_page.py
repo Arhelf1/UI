434d347779ec51da3c39aaf432eedd06469a1997
arhelf1 <44827252+Arhelf1@users.noreply.github.com>
2022-08-02 20:32:14 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,11 +5,9 @@ from .locators import BasePageLocators
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        """go to authorization page"""
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def go_to_disk_page(self):
-        """go to yandex disk page"""
         link = self.browser.find_element(*BasePageLocators.DISK_LINK)
         link.click()
